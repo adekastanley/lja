a16c66ffb0090a028100338eddca0cbd297ea849
adeka stanley <adekastanley2@gmail.com>
2025-08-29 14:48:35 +0100

    fixed mobile nav

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -57,6 +57,48 @@ export const HeroHeader = () => {
 							</button>
 						</div>
 
+						{/* Mobile Menu */}
+						<div
+							className={cn(
+								"bg-background mb-6 w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent",
+								menuState ? "block" : "hidden",
+								"lg:hidden"
+							)}
+						>
+							<div>
+								<ul className="space-y-6 text-base">
+									{menuItems.map((item, index) => (
+										<li key={index}>
+											<Link
+												href={item.href}
+												className="text-muted-foreground hover:text-accent-foreground block duration-150"
+											>
+												<span>{item.name}</span>
+											</Link>
+										</li>
+									))}
+								</ul>
+							</div>
+							<div className="flex w-full flex-col space-y-3 sm:flex-row sm:gap-3 sm:space-y-0 md:w-fit">
+								<Button asChild variant="outline" size="sm">
+									<Link href="#">
+										<span>Jas Fitness</span>
+									</Link>
+								</Button>
+								<Button asChild size="sm">
+									<Link href="#">
+										<span>PPDC NG</span>
+									</Link>
+								</Button>
+								<ModeToggle />
+								<Button asChild size="sm">
+									<Link href="#">
+										<span>Get Started</span>
+									</Link>
+								</Button>
+							</div>
+						</div>
+						{/* Desktop Menu */}
 						<div className="absolute inset-0 m-auto hidden size-fit lg:block">
 							<ul className="flex gap-8 text-sm">
 								{menuItems.map((item, index) => (
