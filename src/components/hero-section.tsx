7670f1cd8dd0ee5f039b5cf3a55da3b980c32def
adeka stanley <adekastanley2@gmail.com>
2025-08-29 14:36:43 +0100

    clean up

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import { ArrowRight, ChevronRight } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { TextEffect } from "./motion-primitives/text-effect";
--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,26 +1,28 @@
-import { cn } from "@/lib/utils";
-import { h5 } from "motion/react-client";
+// import { cn } from "@/lib/utils";
+// import { h5 } from "motion/react-client";
 
-export const Logo = ({
-	className,
-	uniColor,
-}: {
+export const Logo = ({}: // className,
+// uniColor,
+{
 	className?: string;
 	uniColor?: boolean;
 }) => {
 	return <h5 className="font-bold">LJA</h5>;
 };
 
-export const LogoIcon = ({
-	className,
-	uniColor,
-}: {
+export const LogoIcon = ({}: // className,
+// uniColor,
+{
 	className?: string;
 	uniColor?: boolean;
 }) => {
 	return <h5 className="font-bold">LJA</h5>;
 };
 
-export const LogoStroke = ({ className }: { className?: string }) => {
+export const LogoStroke = ({}: // className
+
+{
+	className?: string;
+}) => {
 	return <h5 className="font-bold">LJA</h5>;
 };
